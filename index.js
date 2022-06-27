0baed7c87aaaab231d17dd0b7506197f2b1c5c34
Owen <owen@heha.org>
2022-06-28 00:54:12 +0800

    change from http get method to post when interact with telegram api

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 CHAT_ID = globalThis.TG_CHAT_ID
 TOKEN = globalThis.TG_TOKEN
 
-function addURLOptions(urlstr, options = {}) {
-  let url = urlstr
-  for (const key of Object.keys(options)) {
-    if (options[key]) url += '&' + key + '=' + options[key]
-  }
-  return url
-}
-
 class Handler {
-  constructor(config) {
-    this.chat_id = CHAT_ID
-    this.token = TOKEN
-    this.url = 'https://api.telegram.org/bot' + TOKEN
-  }
 
   async send(req) {
     const formData = await req.formData()
@@ -23,18 +10,21 @@ class Handler {
       body[entry[0]] = entry[1]
     }
 
-    const url = this.getURL("From:" + body['From'] + "\r\n==== Message: ====\r\n" + body['Body'])
-    console.log(await fetch(url))
-    return new Response('', {status: 200})
-  }
+    const msg = {
+      chat_id: CHAT_ID,
+      text: "From:" + body['From'] + "\r\n==== Message: ====\r\n" + body['Body'],
+    }
+    const init = {
+      body: JSON.stringify(msg),
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    }
 
-  getURL(text) {
-    let url = this.url + '/sendMessage?chat_id=' + this.chat_id
-    url = addURLOptions(url, {
-      "text": text,
-      "disable_web_page_preview": true
-    })
-    return url
+    let url = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage'
+    console.log(await fetch(url, init))
+    return new Response('', {status: 200})
   }
 }
 
